f124d018aa0f79113ed88b0aa5f1c42beff55946
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-04-15 21:44:20 +0200

    Actualziado el controlador de usuario

--- src/Project/User/UsersController.php
+++ src/Project/User/UsersController.php
@@ -8,6 +8,8 @@
 
 namespace Project\User;
 
+use DateTime;
+use Firebase\JWT\JWT;
 use Psr\Container\ContainerInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -42,12 +44,37 @@ class UsersController
 		return $response->withJson($user, 200);
 	}
 
-	function update_user($new_id, array $user)
+	function update_user(Request $request, Response $response, array $args)
 	{
-
+		$user_id = $request->getAttribute('token')->id;
 	}
 	function create_user(Request $request, Response $response, array $args)
 	{
+		$post_args = $request->getParsedBody();
+		$id = $this->dao->insert_user(new User(null, $post_args['email'], $post_args['password'], null, 0));
 
+		$user = $this->dao->get_user($post_args['email']);
+		$user->token = $this->generate_token($id);
+
+		return $response->withJson($user, 200);
+
+	}
+	public function generate_token($id)
+	{
+		$now = new DateTime();
+		$future = new DateTime("now +1 year");
+
+		$payload = [
+			"iat" => $now->getTimeStamp(),
+			"exp" => $future->getTimeStamp(),
+			"jti" => base64_encode(random_bytes(16)),
+			'iss' => 'localhost:80',  // Issuer
+			"id" => $id,
+		];
+
+		$secret = 'mysecret';
+		$token = JWT::encode($payload, $secret, "HS256");
+
+		return $token;
 	}
 }
